7eecd5e53f6aa97c8c8fa9e81193e46846a97fa1
ethiraj <ethirajsrinivasan@gmail.com>
2018-03-16 16:17:51 +0800

    Escape Unicode characters

--- app.py
+++ app.py
@@ -36,10 +36,10 @@ def scrap_data(url):
 def clean_scrap_data(data):
     cleaned_documents = []
     for i in data:
-        content = i.text
+        content = i.text.encode('ascii','ignore').decode("utf-8")
         if len(content) == 0: continue
         text = content.split("...")
-        text = [ con.replace("\n", '').replace("\xa0", '') for con in text if len(con) > 0 ] 
+        text = [ con.replace("\n", '').replace("\xa0", '').replace("\"",'') for con in text if len(con) > 0 ] 
         if re.search("^\d{1,2}\s[a-zA-Z]{3}\s\d{4}",text[0]):
             text = text[1:]
         if len(text) == 0: continue
@@ -52,18 +52,12 @@ def clean_scrap_data(data):
 
 
 def get_similarites(documents):
-    gen_docs = [[w.lower() for w in word_tokenize(text)] 
-                for text in documents]
-
+    gen_docs = [[w.lower() for w in word_tokenize(text)] for text in documents]
     dictionary = gensim.corpora.Dictionary(gen_docs)
-
     corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
-
     tf_idf = gensim.models.TfidfModel(corpus)
-    sims = gensim.similarities.Similarity('./',tf_idf[corpus],
-                                          num_features=len(dictionary))
+    sims = gensim.similarities.Similarity('./',tf_idf[corpus], num_features=len(dictionary))
     return sims, dictionary, tf_idf
-
 
 def get_query_doc_tf_idf(query, dictionary, tf_idf):
     query_doc = [w.lower() for w in word_tokenize(query)]
@@ -72,7 +66,6 @@ def get_query_doc_tf_idf(query, dictionary, tf_idf):
 
 def get_similar_docs(sims, query_doc_tf_idf, documents):
     doc_with_rank = {}
-    print(sims[query_doc_tf_idf])
     for i,val in enumerate(sims[query_doc_tf_idf]):
         doc_with_rank[i] = val
     doc_sims = sorted(doc_with_rank.items(),key=lambda x : x[1],reverse=True)[:5]
